c321c93d3277443f38a93265c33a466362568db8
Amha2004 <55863101+Amha2004@users.noreply.github.com>
2019-10-10 20:02:22 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@ if [ -e /usr/bin/wget ]; then
 exit 0;
 fi
 yum -y install wget
-https://raw.githubusercontent.com/Amha2004/NTI300/master/packages.txt
+wget https://raw.githubusercontent.com/Amha2004/NTI300/master/packages.txt
 for packages in $(cat packages.txt); do
 yum -y install $packages
 done
