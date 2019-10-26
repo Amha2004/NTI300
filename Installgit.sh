97f4b25c50c368a1f2978bb18583866cd29f5db8
Amha2004 <55863101+Amha2004@users.noreply.github.com>
2019-10-26 16:16:34 -0700

    Create Installgit.sh

---
+++ Installgit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum -y install git
+git clone https://github.com/Amha2004/NTI300/new/master
+cd NTI300/
+git config --global user.name "Amha2004"
+git config --global user.email amha.getahun@seattlecolleges.edu
